85938a0dfecdb3c091b515138fe7efc79c76ac7e
gitcaiqing <380120634@qq.com>
2018-11-29 18:30:10 +0800

    整合Shiro实现登陆认证

--- src/main/java/com/ssm/controller/HomeController.java
+++ src/main/java/com/ssm/controller/HomeController.java
@@ -3,6 +3,10 @@ package com.ssm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 主页控制器
+ * @author https://blog.csdn.net/caiqing116
+ */
 @Controller
 @RequestMapping("ssm")
 public class HomeController {
